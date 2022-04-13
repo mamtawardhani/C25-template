5f531a9d131c176b8b50fc00484a247ba0380ce9
mamtawardhani <53176352+mamtawardhani@users.noreply.github.com>
2022-04-13 10:11:33 +0530

    Add files via upload

---
+++ ground.js
@@ -0,0 +1,27 @@
+class Ground{
+    constructor(x,y,w,h){
+
+        var options = {
+            isStatic: true
+        }
+        this.w = w
+        this.h = h
+        this.body = Bodies.rectangle(x,y,this.w, this.h, options)
+        World.add(world, this.body)
+    }
+
+    show(){
+        var pos = this.body.position
+        Matter.Body.rotate(this.body,angle)
+
+        push()
+        rectMode(CENTER)
+        translate(pos.x, pos.y)
+        rotate(angle)
+        rect(0,0, this.w, this.h)
+        
+        pop()
+        angle = angle +0.3
+    }
+}
+
